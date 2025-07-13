9bae3305a639e774fd67845df1dee9cf6ef3042e
JeffRisberg <j.s.risberg@gmail.com>
2025-07-13 11:37:10 -0700

    fixed one import

--- fruit_extraction_eval_pipeline.py
+++ fruit_extraction_eval_pipeline.py
@@ -2,8 +2,7 @@ import asyncio
 import json
 import openai
 import weave
-from weave.flow.scorer import MultiTaskBinaryClassificationF1
-
+from weave.scorers import MultiTaskBinaryClassificationF1
 
 # We create a model class with one predict function.
 # All inputs, predictions and parameters are automatically captured for easy inspection.
